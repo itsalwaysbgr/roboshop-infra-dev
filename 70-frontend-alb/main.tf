95914a05781af14ce824e6356d3fd159272c529b
nane <gnane5940@gmail.com>
2025-07-27 10:35:37 +0530

    2025-jul-27

--- 70-frontend-alb/main.tf
+++ 70-frontend-alb/main.tf
@@ -1,17 +1,18 @@
 module "frontend_alb" {
-  source = "terraform-aws-modules/alb/aws"
-  version = "9.16.0"
-  internal = false
-  name    = "${var.project}-${var.environment}-frontend-alb" #roboshop-dev-backend-alb
-  vpc_id  = local.vpc_id
-  subnets = local.public_subnet_ids
-  create_security_group = false
-  security_groups = [local.frontend_alb_sg_id]
+  source                     = "terraform-aws-modules/alb/aws"
+  version                    = "9.16.0"
+  name                       = "${var.project}-${var.environment}-frontend-alb" # roboshop-dev-backend-alb
+  internal                   = false
+  vpc_id                     = local.vpc_id
+  subnets                    = local.public_subnet_ids
+  create_security_group      = false
+  security_groups            = [local.frontend_alb_sg_id]
   enable_deletion_protection = false
+
   tags = merge(
     local.common_tags,
     {
-        Name = "${var.project}-${var.environment}-frontend-alb"
+      Name = "${var.project}-${var.environment}-frontend-alb"
     }
   )
 }
@@ -22,6 +23,7 @@ resource "aws_lb_listener" "frontend_alb" {
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = local.acm_certificate_arn
+
   default_action {
     type = "fixed-response"
 
@@ -35,7 +37,7 @@ resource "aws_lb_listener" "frontend_alb" {
 
 resource "aws_route53_record" "frontend_alb" {
   zone_id = var.zone_id
-  name    = "${var.environment}.${var.zone_name}" #dev.daws86s.site
+  name    = "${var.environment}.${var.zone_name}" # dev.daws86s.site
   type    = "A"
 
   alias {
