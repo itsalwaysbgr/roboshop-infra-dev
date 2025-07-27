c8e761712b69402d3197177b191dc7a86a4eb6e6
nane <gnane5940@gmail.com>
2025-07-27 12:17:59 +0530

    2025-jul-27

---
+++ 91-cdn/variables.tf
@@ -0,0 +1,4 @@
+variable "project"     { default = "roboshop" }
+variable "environment" { default = "dev" }
+variable "zone_id"     { default = "Z0025773HL0IRVDCYXE5" }
+variable "zone_name"   { default = "daws86s.site" }
